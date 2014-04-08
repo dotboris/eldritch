111523d6856bd19c6096c9b360190b9654c1d1f0
Boris Bera <bboris@rsoft.ca>
2014-04-08 19:50:23 -0400

    added example for async block

---
+++ examples/async_block.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'eldritch'
+include Eldritch::DSL
+
+async do
+  puts 'starting long running task'
+  sleep(1)
+  puts 'long running task done'
+end
+
+puts 'doing something else'
+
+# waiting for everyone to finish
+(Thread.list - [Thread.current]).each &:join
