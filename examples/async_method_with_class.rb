1fdf734e18210e61de7036cb7d3e4b1070ac3bdb
Boris Bera <bboris@rsoft.ca>
2014-04-08 19:47:03 -0400

    did the same simplification to class thread example

--- examples/async_method_with_class.rb
+++ examples/async_method_with_class.rb
@@ -5,10 +5,16 @@ include Eldritch::DSL
 class BabysFirstClass
   async def foo(arg)
     puts "Hey I got: #{arg}"
+    sleep(1)
+    puts 'foo done'
   end
 end
 
 obj = BabysFirstClass.new
+
+puts 'calling foo'
 obj.foo('stuff')
-sleep(1)
-puts 'done'
+puts 'doing something else'
+
+# waiting for everyone to stop
+Thread.list.reject{|t| t == Thread.current}.each &:join
