473a0affbbe39e0cc2390365ed81d89cbdf81263
Boris Bera <bboris@rsoft.ca>
2014-04-08 19:35:22 -0400

    since the dirty trick was removed, we don't need to test for it
    Ruby should handle method calls like normal

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -40,12 +40,6 @@ describe Eldritch::DSL do
       end
     end
 
-    context 'with 2+ arguments' do
-      it 'should raise an error' do
-        expect{klass.async(1,2)}.to raise_error(ArgumentError)
-      end
-    end
-
     context 'with 1 argument' do
       before do
         klass.class_eval do
