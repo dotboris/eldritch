9e285b0445116aa3d06177f35c981313ef835b2a
Boris Bera <bboris@rsoft.ca>
2014-04-08 19:19:51 -0400

    added 1 arg context to async test

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -17,76 +17,78 @@ describe Eldritch::DSL do
     end
   end
 
-  describe '#async(method)' do
-    before do
-      klass.class_eval do
-        async def foo; end
-      end
-    end
-
-    it 'should create a __async method' do
-      expect(klass.new).to respond_to(:__async_foo)
-    end
-
-    it 'should redefine the method' do
-      expect(klass).to receive(:define_method).with(:foo)
-
-      klass.class_eval do
-        async def foo; end
-      end
-    end
-
-    describe 'async method' do
-      it 'should call the original' do
-        allow(Thread).to receive(:new).and_yield(double(:task).as_null_object)
-
-        instance = klass.new
-        expect(instance).to receive(:__async_foo)
-
-        instance.foo
+  describe '#async' do
+    context 'with 1 argument' do
+      before do
+        klass.class_eval do
+          async def foo; end
+        end
       end
 
-      it 'should pass all arguments' do
-        allow(Thread).to receive(:new).and_yield(double(:task).as_null_object)
+      it 'should create a __async method' do
+        expect(klass.new).to respond_to(:__async_foo)
+      end
+
+      it 'should redefine the method' do
+        expect(klass).to receive(:define_method).with(:foo)
 
         klass.class_eval do
-          async def foo(_,_,_); end
+          async def foo; end
         end
-        instance = klass.new
-        expect(instance).to receive(:__async_foo).with(1,2,3)
-
-        instance.foo(1,2,3)
       end
 
-      it 'should set the task value' do
-        task = double(:task)
-        expect(task).to receive(:value=).with(42)
-        allow(Thread).to receive(:new).and_yield(task)
+      describe 'async method' do
+        it 'should call the original' do
+          allow(Thread).to receive(:new).and_yield(double(:task).as_null_object)
 
-        klass.class_eval do
-          async def foo; 42; end
+          instance = klass.new
+          expect(instance).to receive(:__async_foo)
+
+          instance.foo
         end
-        instance = klass.new
 
-        instance.foo
-      end
+        it 'should pass all arguments' do
+          allow(Thread).to receive(:new).and_yield(double(:task).as_null_object)
 
-      it 'should return a task' do
-        allow(Eldritch::DSL).to receive(:new).and_return(double(:task).as_null_object)
+          klass.class_eval do
+            async def foo(_,_,_); end
+          end
+          instance = klass.new
+          expect(instance).to receive(:__async_foo).with(1,2,3)
 
-        instance = klass.new
+          instance.foo(1,2,3)
+        end
 
-        expect(instance.foo).to be_a(Eldritch::Task)
-      end
+        it 'should set the task value' do
+          task = double(:task)
+          expect(task).to receive(:value=).with(42)
+          allow(Thread).to receive(:new).and_yield(task)
 
-      it 'should start the task' do
-        task = double(:task)
-        expect(task).to receive(:start).once
-        allow(Eldritch::Task).to receive(:new).and_return(task)
+          klass.class_eval do
+            async def foo; 42; end
+          end
+          instance = klass.new
 
-        instance = klass.new
+          instance.foo
+        end
 
-        instance.foo
+        it 'should return a task' do
+          allow(Eldritch::DSL).to receive(:new).and_return(double(:task).as_null_object)
+
+          instance = klass.new
+
+          expect(instance.foo).to be_a(Eldritch::Task)
+        end
+
+        it 'should start the task' do
+          task = double(:task)
+          expect(task).to receive(:start).once
+          allow(Eldritch::Task).to receive(:new).and_return(task)
+
+          instance = klass.new
+
+          instance.foo
+        end
       end
     end
   end
