92c68fa47335811995fda9d4b9ac53fc7d3fb290
Boris Bera <bboris@rsoft.ca>
2014-04-18 19:03:03 -0400

    documented dsl.rb

--- lib/eldritch/dsl.rb
+++ lib/eldritch/dsl.rb
@@ -1,7 +1,36 @@
 module Eldritch
+  # Provides DSL for:
+  # - {#async async methods}
+  # - {#async async blocks}
+  # - {#together together blocks}
+  # - {#sync sync keyword}
   module DSL
     using Eldritch::Refinements
 
+    # Creates an asynchronous method or starts an async block
+    #
+    # If a block is passed, this will be an async block.
+    # Otherwise this method will create an async method.
+    #
+    # When an async block is called, it will yield the block in a new thread.
+    #
+    #   async do
+    #     # will run in parallel
+    #   end
+    #   #=> <Task>
+    #
+    # When called, async methods behave exactly like async blocks.
+    #
+    #   async def foo
+    #     # will run in parallel
+    #   end
+    #
+    #   foo
+    #   #=> <Task>
+    #
+    # @param [Symbol] method the name of the async method.
+    # @return [Task] a task representing the async method or block
+    #   (only for async block and async method call)
     def async(method=nil, &block)
       if block
         async_block(&block)
@@ -10,19 +39,51 @@ module Eldritch
       end
     end
 
+    # Allows async methods to be called like synchronous methods
+    #
+    #   sync send_email(42) # send_mail is async
+    #
+    # @param [Task] task a task returned by {#async}
+    # @return whatever the method has returned
     def sync(task)
       task.value
     end
 
+    # Creates a group of async call and blocks
+    #
+    # When async blocks and calls are inside a together block, they can act as a group.
+    #
+    # A together block waits for all the async call/blocks that were started within itself to stop before continuing.
+    #
+    #   together do
+    #     5.times do
+    #       async { sleep(1) }
+    #     end
+    #   end
+    #   # waits for all 5 async blocks to complete
+    #
+    # A together block will also yield a {Group}. This can be used to interact with the other async calls/blocks.
+    #
+    #   together do |group|
+    #     5.times do
+    #       async do
+    #         # stop everyone else
+    #         group.interrupt if something?
+    #       end
+    #     end
+    #   end
+    #
+    # @yield [Group] group of async blocks/calls
+    # @see Group Group class
     def together
       old = Thread.current.group
 
-      t = Group.new
-      Thread.current.group = t
+      group = Group.new
+      Thread.current.group = group
 
-      yield t
+      yield group
 
-      t.wait_all
+      group.wait_all
       Thread.current.group = old
     end
 
