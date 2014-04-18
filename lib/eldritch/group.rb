58b90897944d9c1e9db6ba59f6accf756d843ba7
Boris Bera <bboris@rsoft.ca>
2014-04-18 19:50:24 -0400

    documented group

--- lib/eldritch/group.rb
+++ lib/eldritch/group.rb
@@ -1,6 +1,8 @@
 require 'thread'
 
 module Eldritch
+  # Represents a group of {Task tasks} or {DSL#async async calls/block}.
+  # It is used to act upon all the tasks in the group.
   class Group
     using Eldritch::Refinements
 
@@ -10,6 +12,7 @@ module Eldritch
       @accept = true
     end
 
+    # @return [Array<Task>] the other async calls/blocks in the group
     def others
       @tasks - [Thread.current.task]
     end
@@ -26,6 +29,9 @@ module Eldritch
       task.start if accept
     end
 
+    # Yields the block in mutual exclusion with all the async calls/tasks
+    #
+    # @yield
     def synchronize(&block)
       @mutex.synchronize { block.call }
     end
@@ -34,6 +40,11 @@ module Eldritch
       @tasks.each {|t| t.wait}
     end
 
+    # Aborts the other async calls/blocks in the group
+    #
+    # *Warning*: This call will directly kill underlying threads. This isn't very safe.
+    #
+    # @see Task#abort
     def abort
       @mutex.synchronize do
         @accept = false
@@ -41,6 +52,11 @@ module Eldritch
       end
     end
 
+    # Interrupts the other async calls/blocks in the group
+    #
+    # Interruptions are done using exceptions that can be caught by the async calls/blocks to perform cleanup.
+    #
+    # @see Task#interrupt
     def interrupt
       @mutex.synchronize do
         @accept = false
