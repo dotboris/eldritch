7a97bb8360a3349948e6bb9280d4044672454bb9
Boris Bera <bboris@rsoft.ca>
2014-04-18 13:59:29 -0400

    thread_spec uses group for variables and such

--- spec/thread_spec.rb
+++ spec/thread_spec.rb
@@ -6,7 +6,7 @@ using Eldritch::Refinements
 describe Thread do
   let(:thread) { Thread.new {} }
 
-  it 'should have together accessor' do
+  it 'should have group accessor' do
     # refinements don't work with #respond_to? and send, we have to check for errors
     expect{thread.group}.not_to raise_error
     expect{thread.group = nil}.not_to raise_error
@@ -18,30 +18,30 @@ describe Thread do
     expect{thread.task = nil}.not_to raise_error
   end
 
-  describe '#together' do
+  describe '#group' do
     it 'should return the togther previously set' do
-      together = double('together')
-      thread.group = together
-      expect(thread.group).to eql(together)
+      group = double('group')
+      thread.group = group
+      expect(thread.group).to eql(group)
     end
 
-    it 'should return a NilTogether when none are set' do
+    it 'should return a NilGroup when none are set' do
       expect(thread.group).to be_a Eldritch::NilGroup
     end
   end
 
-  describe '#together?' do
-    it 'should be false when together is nil' do
+  describe '#in_group?' do
+    it 'should be false when group is nil' do
       thread.group = nil
       expect(thread.in_group?).to be_false
     end
 
-    it 'should be false when together is a NilTogether' do
+    it 'should be false when group is a NilGroup' do
       thread.group = Eldritch::NilGroup.new
       expect(thread.in_group?).to be_false
     end
 
-    it 'should be true when together is set' do
+    it 'should be true when group is set' do
       thread.group = 2
       expect(thread.in_group?).to be_true
     end
