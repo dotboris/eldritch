497378e6ad275ab543e5880fd6b9d27af51098a0
Boris Bera <bboris@rsoft.ca>
2014-04-18 12:38:56 -0400

    renamed ext_core folder to refinements

--- lib/eldritch/safe.rb
+++ lib/eldritch/safe.rb
@@ -1,7 +1,7 @@
 require 'eldritch/version'
 require 'eldritch/task'
 require 'eldritch/dsl'
-require 'eldritch/ext_core/thread'
+require 'eldritch/refinements/thread'
 require 'eldritch/together'
 require 'eldritch/interrupted_error'
 
