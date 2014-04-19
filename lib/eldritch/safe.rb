e32ecb458460a1156f5262c6192e7cd003c88803
Boris Bera <bboris@rsoft.ca>
2014-04-18 20:11:34 -0400

    documented inject_dsl

--- lib/eldritch/safe.rb
+++ lib/eldritch/safe.rb
@@ -6,6 +6,19 @@ require 'eldritch/group'
 require 'eldritch/interrupted_error'
 
 module Eldritch
+  # Injects the DSL in the main
+  #
+  # This is automatically called when you call
+  #   require 'eldritch'
+  #
+  # If you do not want to contaminate the main you can require +eldritch/safe+ and
+  # include or extend Eldricth::DSL yourself.
+  #
+  #   require 'eldritch/safe'
+  #   module Sandbox
+  #     include Eldritch::DSL  # for async blocks, together and sync
+  #     extend Eldritch::DSL   # for async method declaration
+  #   end
   def self.inject_dsl
     Object.include Eldritch::DSL
   end
