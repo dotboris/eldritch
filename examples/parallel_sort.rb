765bb73158c280219e57727d4a456f39df95e41c
Boris Bera <bboris@rsoft.ca>
2014-04-24 17:41:36 -0400

    removed the not_parallel sort
    The goal of these examples is to show how the blocks work not to time
     the code. Also, we're going to lose no matter what. The ruby GIL
     will make this whole thing moot.

--- examples/parallel_sort.rb
+++ examples/parallel_sort.rb
@@ -16,37 +16,20 @@ def merge(a, b)
   merged + (a.empty? ? b : a)
 end
 
-def parallel_sort(array)
+def parallel_merge_sort(array)
   return array if array.size <= 1
 
   mid = (array.length / 2).floor
 
-  first = async { parallel_sort(array.slice(0, mid)) }
-  second = parallel_sort(array.slice(mid, array.length - mid))
+  first = async { parallel_merge_sort(array.slice(0, mid)) }
+  second = parallel_merge_sort(array.slice(mid, array.length - mid))
 
   merge(second, first.value)
 end
 
-def not_parallel_sort(array)
-  return array if array.size <= 1
+nums = (1..25).to_a.shuffle
 
-  mid = (array.length / 2).floor
-
-  first = not_parallel_sort(array.slice(0, mid))
-  second = not_parallel_sort(array.slice(mid, array.length - mid))
-
-  merge(second, first)
-end
-
-nums = 100000.times.map { rand(1..100000) }
-
-start = Time.now
-parallel_sort(nums)
-mid = Time.now
-not_parallel_sort(nums)
-stop = Time.now
-
-puts 'Time parallel'
-puts mid - start
-puts 'Time sequential'
-puts stop - mid
+puts "values: #{nums.join(', ')}"
+puts 'sorting...'
+sorted = parallel_merge_sort(nums)
+puts "values: #{sorted.join(', ')}"
