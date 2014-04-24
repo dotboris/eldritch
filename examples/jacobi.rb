1857b00c759722874c9fa5cc765a08377596baf6
Boris Bera <bboris@rsoft.ca>
2014-04-24 16:33:57 -0400

    renamed width and height

--- examples/jacobi.rb
+++ examples/jacobi.rb
@@ -22,8 +22,8 @@ epsilon = 0.001
 print_matrix matrix
 puts
 
-matrix_height = matrix.length - 2
-matrix_width = matrix[0].length - 2
+height = matrix.length - 2
+width = matrix[0].length - 2
 
 max_diff = epsilon + 1
 iterations = 1
@@ -31,9 +31,9 @@ while max_diff > epsilon do
   matrix_temp = Marshal.load(Marshal.dump(matrix))
 
   together do
-    (1..matrix_height).each do |row_id|
+    (1..height).each do |row_id|
       async do
-        (1..matrix_width).each do |col_id|
+        (1..width).each do |col_id|
           neighbors = [col_id - 1, col_id + 1].product([row_id - 1, row_id + 1]).map{|i, j| matrix[i][j]}
           matrix_temp[row_id][col_id] = neighbors.reduce(:+) / 4.0
         end
@@ -41,8 +41,8 @@ while max_diff > epsilon do
     end
   end
 
-  max_diff = (1..matrix_height).to_a
-    .product((1..matrix_width).to_a)
+  max_diff = (1..height).to_a
+    .product((1..width).to_a)
     .map { |i| (matrix_temp[i[0]][i[1]] - matrix[i[0]][i[1]]).abs }
     .max
   matrix = matrix_temp
@@ -50,5 +50,6 @@ while max_diff > epsilon do
   print_matrix matrix
   puts "iteration = #{iterations}; diff = #{max_diff}"
   puts
+
   iterations+=1
 end
